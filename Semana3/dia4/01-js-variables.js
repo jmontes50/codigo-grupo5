e30f14023885d9b1577a05c6bb3254b51d63664e
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-11-12 22:15:31 -0500

    Javascript para pollitos

---
+++ Semana3/dia4/01-js-variables.js
@@ -0,0 +1,18 @@
+// VARIABLES EN JAVASCRIPT
+// Variable de tipo number o numerica
+let edad = 19;
+let sueldo = 950.5;
+// Variables de tipo de cadena de texto
+let nombre = "Jorge Luis Garnica Blanco";
+let mensaje = "Se que estás ahí Técnico de Movistar choreando mi wifi";
+// Varibles de tipo booleana
+let casado = false;
+let cazado = true;
+// Variables de tipo arreglo o array
+let platos = ["Cebiche", "Seco de Codero", "Rocoto Relleno"];
+
+// console.log()
+// imprimir en la consola del navegador
+console.log("algo como ésto");
+console.log(cazado);
+console.log("casado " + casado);
