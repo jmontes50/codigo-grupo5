da865c694b92ab9246c18e6cafd3fe788b6656cf
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-11-14 07:45:23 -0500

    subiendo libro de algoritmos resueltos

--- Semana3/dia4/01-js-variables.js
+++ Semana3/dia4/01-js-variables.js
@@ -16,3 +16,20 @@ let platos = ["Cebiche", "Seco de Codero", "Rocoto Relleno"];
 console.log("algo como ésto");
 console.log(cazado);
 console.log("casado " + casado);
+
+// plantillas o templates usando `backticks`
+console.log(`Mi nombre es: ${nombre} y tengo ${edad} años`);
+
+// modificar el valor de una variable ya creada
+edad = 29;
+console.log(`Corrección!, mi edad es de ${edad} años`);
+
+console.log(`Mis platos favoritos son ${platos}`);
+console.log(platos);
+
+let mensajeCompleto = `Mi nombre es ${nombre} y tengo ${edad} años`;
+console.log(mensajeCompleto);
+
+/// constantes se crean con el identificador "const"
+const pi = 3.1416;
+console.log(`Valor de PI = ${pi}`);
