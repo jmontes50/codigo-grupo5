320b988c342d21c9c73e266864d2069d4966064c
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-11-23 20:25:15 -0500

    funciones de los arreglos e intro de objetos

---
+++ Semana5/dia1/05-objetos.js
@@ -0,0 +1,9 @@
+let objPersona = {
+  nombre: "Jorgito",
+  direccion: "Calle Siempre viva",
+  platosFavoritos: ["Cebiche", "Rocoto Relleno", "Ají de Gashina"],
+  objUbicacion: {
+    lat: -70.1324,
+    lon: -16.5135
+  }
+};
