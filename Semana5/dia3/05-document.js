1fabdc003aafe16420a2607106deb59adb78af76
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-11-26 20:37:20 -0500

    API del objeto  HTMLElement

--- Semana5/dia3/05-document.js
+++ Semana5/dia3/05-document.js
@@ -29,6 +29,30 @@ let lis = document.getElementsByClassName("lista__item");
  */
 let arreglo = Array.from(lis);
 console.log(arreglo);
-arreglo.forEach((li) => {
-  console.log(li);
+arreglo.forEach((li, i) => {
+  li.innerText = `List item ${i}`;
+})
+
+/**
+ * document.querySelector("");
+ * Retorna un (01) elemento HTML que coincida con el
+ * patrón de selección como si fuera mediante CSS
+ */
+
+let subtitulo = document.querySelector("#subtitulo");
+subtitulo.style.textDecoration = "underline";
+
+/**
+ * document.querySelectorAll("");
+ * Retorna una colección de elementos HTML que coincida
+ * con el patrón de selecicón como si fuera mediante CSS
+ */
+
+let cajitas = document.querySelectorAll(".cajita");
+cajitas = Array.from(cajitas);
+cajitas.forEach((caja) => {
+  caja.style.height = "20px";
+  caja.style.width = "20px";
+  caja.style.backgroundColor = "#444";
+  caja.style.borderRadius = "50%";
 })
