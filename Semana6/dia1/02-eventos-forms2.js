74fe39409b5f8b521da236dfdd15f273b06f0a85
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-11-30 20:31:20 -0500

    ejercicio2 de eventos

---
+++ Semana6/dia1/02-eventos-forms2.js
@@ -0,0 +1,32 @@
+const tipos = [
+  { idTipo: 1, nombreTipo: "Platos" },
+  { idTipo: 2, nombreTipo: "Bebidas" },
+  { idTipo: 3, nombreTipo: "Postres" }
+];
+
+const categorias = [
+  {
+    idCategoria: 1, nombreCategoria: "Fondo", idTipo: 1
+  },
+  {
+    idCategoria: 2, nombreCategoria: "Entradas", idTipo: 1
+  },
+  {
+    idCategoria: 3, nombreCategoria: "Sopas", idTipo: 1
+  },
+  {
+    idCategoria: 4, nombreCategoria: "Cocktails", idTipo: 2
+  },
+  {
+    idCategoria: 5, nombreCategoria: "Refrescos", idTipo: 2
+  },
+  {
+    idCategoria: 6, nombreCategoria: "Jugo de Frutas", idTipo: 2
+  },
+  {
+    idCategoria: 7, nombreCategoria: "Helados", idTipo: 3
+  },
+  {
+    idCategoria: 8, nombreCategoria: "Tortas", idTipo: 3
+  }
+]
