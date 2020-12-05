6a49b2b6dbbbfd915560ab03f31b5e2882e1bf97
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-12-04 19:18:01 -0500

    datatblaes basico

--- Semana6/dia5/01-jQuery/01-jquery.js
+++ Semana6/dia5/01-jQuery/01-jquery.js
@@ -1,5 +1,18 @@
 let parrafos = $(".miParrafo");
-console.log(parrafos);
+parrafos.addClass("cursiva");
+
+parrafos[0].style.color = "red";
+parrafos[0].innerHTML = "Nuevo primer párrafo";
+/**
+ * jquery.html("texto")
+ * Funciona como el innerHTML para un grupo de elementos
+ */
+parrafos.html("Nuevos textos de los párrafos")
+
+$("#header").click(function (e) { 
+  e.preventDefault();
+  console.log("Se hizo click en el header");
+});
 
 let body = $("body");
 console.log(body);
