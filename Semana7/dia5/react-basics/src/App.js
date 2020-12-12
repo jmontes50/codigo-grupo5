37a42138430dc9768f690c0fe0fdca411275197a
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-12-11 22:06:00 -0500

    Componentes en react

--- Semana7/dia5/react-basics/src/App.js
+++ Semana7/dia5/react-basics/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import Header from "./Header";
-
+import Footer from "./Footer";
 const App = () => {
 
+  const miTitulo = "El título de mi apliqueishon";
+  const libros = ["Cronica de una muerte anunciada", "Ña catita", "Ensayo sobre la ceguera", "Hansel y Gretel", "Siete Semillas", "Narraciones", "La rebelión de Atlas", "El sexto"];
+  let librosLis = libros.map(libro => {
+    return <li>{libro}</li>
+  });
 
 
   return (
     <>
-      <Header></Header>
-      <div>
-        PRIMER DIV
-      </div>
+      <Header />
+      <h1>{miTitulo}</h1>
       <div>
         Hola Mundo
       <p>
           lorem
       </p>
         <ul>
-          <li>asdasd</li>
+          {librosLis}
         </ul>
       </div>
+      <Footer />
     </>
   )
 }
