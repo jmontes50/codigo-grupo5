d17ecf1e98bb8811df66c246a10053efb6782d79
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-12-15 18:44:05 -0500

    Mostrando datos de cada clima

--- Semana8/dia1/react-clima/src/components/CardClima.js
+++ Semana8/dia1/react-clima/src/components/CardClima.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
 const CardClima = ({ objClima }) => {
+
+  let fechaYHoraString = "";
+  let unix = new Date(objClima.dt * 1000);
+  fechaYHoraString = unix.getDate() + "/" + (unix.getMonth() + 1) + "/" + unix.getFullYear()
+    + " " + unix.getHours() + ":" + unix.getMinutes();
+
+
   return (
-    <div>
-      {
-        objClima.dt
-      }
+    <div className="card shadow">
+      <div className="card-body">
+        <div className="row">
+          <div className="col-md-6">
+            <p className="display-4 text-center">
+              {objClima.main.temp} &#8451;
+            </p>
+          </div>
+          <div className="col-md-6 flex-centrado">
+            <img src="http://placehold.it/60X60" alt="" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <p><small className="text-muted">Fecha y Hora:</small></p>
+            <p><strong> {fechaYHoraString}</strong></p>
+          </div>
+          <div className="col-md-4">
+            
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
     </div>
   )
 }
